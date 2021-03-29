98128460c0272307c5fbfcfac757dbe35d445be6
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-03-29 10:52:45 +0530

    add urls and views

--- crm_project/accounts/views.py
+++ crm_project/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('home')
+
+def customer(request):
+    return HttpResponse('customer')
+
+def product(request):
+    return HttpResponse('product')
