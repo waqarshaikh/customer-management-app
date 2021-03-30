227f7e27d40c1301cd3a5c52e8a1f484f70bf6fe
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-03-30 21:21:12 +0530

    Rendering data to tempelate

--- crm_project/accounts/views.py
+++ crm_project/accounts/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_orders = orders.count()
+    total_customers = customers.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 
+        'total_orders': total_orders, 'total_customers': total_customers,
+        'delivered': delivered, 'pending': pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def customer(request):
-    return render(request, 'accounts/customer.html')
+    customers = Customer.objects.all()
+    return render(request, 'accounts/customer.html', {'customers': customers})
 
 def product(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
