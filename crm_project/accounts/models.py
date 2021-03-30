865e3f090465e05d934c60cabecfa529e9f56b6e
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-03-30 16:13:51 +0530

    Database and Admin Panel

--- crm_project/accounts/models.py
+++ crm_project/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=20, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=250, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    status = models.CharField(max_length=20, null=True,  choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
