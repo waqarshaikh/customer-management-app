b1d941ed2b52266b8406ac402d2ab3ff6238c954
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-04-01 23:16:33 +0530

    Add CRUD

--- crm_project/accounts/models.py
+++ crm_project/accounts/models.py
@@ -42,3 +42,6 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=20, null=True,  choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.product.name
--- crm_project/accounts/urls.py
+++ crm_project/accounts/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products', views.product, name='products'),
-    path('customer/<str:id>', views.customer, name='customer'),
+    path('products/', views.product, name='products'),
+    path('customer/<id>', views.customer, name='customer'),
+    path('create_order/', views.createOrder, name='create_order'),
+    path('update_order/<id>', views.updateOrder, name='update_order'),
+    path('delete_order/<id>', views.deleteOrder, name='delete_order'),
 ]
