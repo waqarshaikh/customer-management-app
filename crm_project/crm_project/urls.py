b7c777145320adc0212f3e0118e8f705deb21804
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-04-28 13:06:41 +0530

    create customer_feedback.html template and urls.py

--- crm_project/crm_project/urls.py
+++ crm_project/crm_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('customer_feedback', include('customer_feedback.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
