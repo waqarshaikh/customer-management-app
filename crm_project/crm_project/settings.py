1470481d56612a82716b9c25041488fd78598fa5
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-04-28 13:05:35 +0530

    add customer_feedback app to INSTALLED_APPS

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'customer_feedback.apps.CustomerFeedbackConfig',
     'django_filters',
 ]
 
