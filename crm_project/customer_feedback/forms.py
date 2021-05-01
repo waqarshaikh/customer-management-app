aae6014db1c2541af69e11256b0200fa737274bf
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-05-01 22:42:33 +0530

    add intrested_customer

--- crm_project/customer_feedback/forms.py
+++ crm_project/customer_feedback/forms.py
@@ -1,7 +1,21 @@
+from accounts.models import Product
+from django.forms.widgets import CheckboxSelectMultiple
 from django.forms import ModelForm
-from .models import CustomerFeedback
+from .models import CustomerFeedback, InterstedCustomer
 
 class CustomerFeedbackForm(ModelForm):
     class Meta:
         model = CustomerFeedback
         fields = '__all__'
+
+class InterstedCustomerForm(ModelForm):
+    class Meta:
+        model = InterstedCustomer
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        
+        super(InterstedCustomerForm, self).__init__(*args, **kwargs)
+
+        self.fields["product"].widget = CheckboxSelectMultiple()
+        self.fields["product"].queryset = Product.objects.all()
