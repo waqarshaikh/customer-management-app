a5d90786a382267efc59a4c21b64a50364d8793d
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-05-22 15:31:30 +0530

    Update Employee Model and Create Customer Model

--- crm_project/accounts/models.py
+++ crm_project/accounts/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Employee(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    email = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    profile_pic = models.ImageField(null=True, blank=True, default='default-profile-pic.jpg')
+
+    def __str__(self):
+        return str(self.name)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    employee = models.ForeignKey(Employee, null=True, blank=True, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, null=True)
     email = models.CharField(max_length=20, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
