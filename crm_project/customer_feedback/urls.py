bd90f3c00d62a9f1c36f349066ab006adadb89cc
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-05-26 20:32:55 +0530

    merge all 3 forms in a single template

--- crm_project/customer_feedback/urls.py
+++ crm_project/customer_feedback/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.customer_feedback, name='customer-feedback'),
+    path('', views.customer_feedback_form, name='customer-feedback-form'),
+    path('customer_feedback/', views.customer_feedback, name='customer_feedback'),
     path('intrested_customer/', views.intrested_customer, name='intrested_customer'),
     path('customer_complaint/', views.customer_complaint, name='customer_complaint'),
 ]
