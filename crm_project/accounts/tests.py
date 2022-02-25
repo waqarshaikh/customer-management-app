4e6f2a553404fb7dc2dac5067329910d215de849
Waqar <waqar@incubyte.co>
2022-02-25 23:24:42 +0530

    Setup Unit Testing

--- crm_project/accounts/tests.py
+++ crm_project/accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 class URLTests(TestCase):
-    def test_homepage(self):
+    def test_homepage_redirects_to_loginpage(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/login/?next=/')
-        
