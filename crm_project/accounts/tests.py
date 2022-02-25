a4ba850d914549c3dd6ba30e7999175c05d37935
Waqar <waqar@incubyte.co>
2022-02-25 23:23:56 +0530

    setting up unit testing

--- crm_project/accounts/tests.py
+++ crm_project/accounts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
-
-# Create your tests here.
+class URLTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.url, '/login/?next=/')
+        
